b1a1ba25c0e793213cd4dbb31995f9166f1750d0
Jonus <joclapshaw@gmail.com>
2026-04-18 13:47:22 -0700

    Finding the starting location

--- src/ExplorerSearchTest.java
+++ src/ExplorerSearchTest.java
@@ -17,4 +17,46 @@ public class ExplorerSearchTest {
 
     // Add more tests here!
     // Come up with varied cases
+
+    @Test
+    public void testStartingLocation_TopLeft() {
+        int[][] island = {
+            {0,1,1,3,1,1},
+            {3,2,3,1,3,1},
+            {1,1,1,1,3,3},
+            {3,1,2,1,1,1},
+            {1,1,1,2,1,1},
+        };
+
+        int[] actual = ExplorerSearch.startingLocation(island);
+        assertArrayEquals(new int[]{0, 0}, actual);
+    }
+
+    @Test
+    public void testStartingLocation_Middle() {
+        int[][] island = {
+            {1,1,1,3,1,1},
+            {3,2,3,1,3,1},
+            {1,1,1,0,3,3},
+            {3,1,2,1,1,1},
+            {1,1,1,2,1,1},
+        };
+
+        int[] actual = ExplorerSearch.startingLocation(island);
+        assertArrayEquals(new int[]{2, 3}, actual);
+    }
+
+    @Test
+    public void testStartingLocation_LastIndex() {
+        int[][] island = {
+            {1,1,1,3,1,1},
+            {3,2,3,1,3,1},
+            {1,1,1,1,3,3},
+            {3,1,2,1,1,1},
+            {1,1,1,2,1,0},
+        };
+
+        int[] actual = ExplorerSearch.startingLocation(island);
+        assertArrayEquals(new int[]{4, 5}, actual);
+    }
 }
